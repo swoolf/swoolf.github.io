dd2805ac5cbde77fea662469b80d4979fcff8b42
s woolf <swoolf@ss-Air-2.lan>
2018-02-07 18:31:58 -0500

    harmono

--- drawing/harmonograph.js
+++ drawing/harmonograph.js
@@ -7,12 +7,18 @@ var harmonographClass= function(canvas, canvasTag, canvasSize){
     this._canvasTag=canvasTag;
     this._canvasSize=canvasSize;
     this._pendulums=new Array();
+    this._pendulumsIn=new Array();
+    this._pendulumsFin=new Array();
     
-    this._timeLimit = 2000;
+    this.ratio=0;
+    this.goingUp=true;
+    this.count=0;
+    
+    this._timeLimit = 1000;
     this._timeStep=0.01;
     this._drawAtOnce=0.2;
     
-    this._amplitudeCutOff = 10;
+    this._amplitudeCutOff = 20;
     this._drawSpeed=10;
     
     this._canvas.lineWidth=0.2;
@@ -21,6 +27,15 @@ var harmonographClass= function(canvas, canvasTag, canvasSize){
     this.addPendulum=function(amp,freq,phase,damping,plane){
 		var p=new PendulumClass(amp,freq,phase,damping,plane);
 		this._pendulums.push(p);
+	}
+    
+    this.addPendulumIn=function(amp,freq,phase,damping,plane){
+		var p=new PendulumClass(amp,freq,phase,damping,plane);
+		this._pendulumsIn.push(p);
+	}
+    this.addPendulumFin=function(amp,freq,phase,damping,plane){
+		var p=new PendulumClass(amp,freq,phase,damping,plane);
+		this._pendulumsFin.push(p);
 	}
     
     this.draw=function(){
@@ -42,8 +57,6 @@ var harmonographClass= function(canvas, canvasTag, canvasSize){
 				}
 			}
 			
-			
-			
 			if(t==0){
 				this._canvas.moveTo(x,y);
 			}else{
@@ -58,7 +71,68 @@ var harmonographClass= function(canvas, canvasTag, canvasSize){
         
 	}
     
-	
+    
+    this.drawChange=function(){
+        thisObject._canvas.clearRect(0,0,thisObject._canvasSize[0],thisObject._canvasSize[1]);
+		thisObject._canvas.beginPath();
+		thisObject._pendulums = thisObject.interPen(thisObject._pendulumsIn, thisObject._pendulumsFin, thisObject.ratio, thisObject.count);
+        
+//        console.log(thisObject._pendulums);
+        
+		var centreX=thisObject._canvasSize[0]/2
+		var centreY=thisObject._canvasSize[1]/2
+		var t=0;
+		while(thisObject._stillOscilating(t)){
+			var x=centreX;
+			var y=centreY;
+			
+			for(var i=0;i<thisObject._pendulums.length;i++){
+				if(thisObject._pendulums[i].plane=='x'){
+					x+=thisObject._pendulums[i].path(t);
+				}else{
+					y+=thisObject._pendulums[i].path(t);
+				}
+			}
+			
+			if(t==0){
+				thisObject._canvas.moveTo(x,y);
+			}else{
+				thisObject._canvas.lineTo(x,y);
+			}
+			
+			t+=thisObject._timeStep;
+		}
+        
+        console.log(thisObject.ratio);
+		thisObject._canvas.stroke();
+        if (thisObject.ratio >1){	thisObject.goingUp = false; }
+        if (thisObject.ratio<=0.05){
+            if (!thisObject.goingUp){
+                thisObject.count += 1;
+                if(thisObject.count >= 3){thisObject.count=0;}
+                }
+            thisObject.goingUp=true;                    
+            }
+        
+        if (thisObject.goingUp){ thisObject.ratio+=0.02; }
+        else                    {thisObject.ratio+= -.08; }
+        setTimeout(thisObject.drawChange,10);
+    }
+    
+	this.interPen=function(initPens, finalPens, ratio, num){
+        var curPens = new Array();
+        for(var i=0;i<initPens.length;i++){
+            var amp = initPens[i].a + (finalPens[i+num*4].a - initPens[i].a)*ratio;
+            var freq = initPens[i].f + (finalPens[i+num*4].f - initPens[i].f)*ratio;
+            var phase = initPens[i].p + (finalPens[i+num*4].p - initPens[i].p)*ratio;
+            var dec = initPens[i].d + (finalPens[i+num*4].d - initPens[i].d)*ratio;
+            var plane = initPens[i].plane;
+            curPens.push(new PendulumClass(amp, freq, dec, phase, plane));
+        }
+//        console.log(curPens[2].a);
+        return curPens;
+    }
+    
 	this.drawSlow=function(){
 		
 //		this._cancelDrawSlow();
--- projects/_posts/2018-02-06-harmonograph.md
+++ projects/_posts/2018-02-06-harmonograph.md
@@ -1,9 +1,16 @@
-<!DOCTYPE html>
-<html>
-    <head>
-    <script type="text/javascript" src="harmonograph.js"></script>
-    </head>
-<body>
+---
+layout: post
+title: "Harmonographs"
+postorder: 4
+tags: JS pendulums
+image: assets/images/project-images/harmono/cover.jpg
+icon: assets/images/project-images/harmono/icon.png
+description: >
+  the only perscription is...
+---
+<script type="text/javascript" src="/drawing/harmonograph.js"></script>
+I built these as a reason to play with JS classes and canvases. 
+Each of these can, in theory, be drawn with a harmonograph. Enjoy. 
 
 <canvas id="myCanvas" width="600" height="600"
 style="border:1px solid #000000;">
@@ -31,10 +38,15 @@ style="border:1px solid #000000;">
     harmo.addPendulumFin(100.0,0.07,0.01,2.0,'x');
     harmo.addPendulumFin(100.0,0.08,0.05,1.0,'y');
     
-    harmo.addPendulumFin(200.0,3.57,0.01,1.0,'x');
-    harmo.addPendulumFin(200.0,3.571,0.0,1.5,'y');
-    harmo.addPendulumFin(100.0,0.07,0.01,0.0,'x');
-    harmo.addPendulumFin(100.0,0.08,0.05,1.0,'y');
+//    harmo.addPendulumFin(200.0,3.57,0.01,1.0,'x');
+//    harmo.addPendulumFin(200.0,3.571,0.0,1.5,'y');
+//    harmo.addPendulumFin(100.0,0.07,0.01,0.0,'x');
+//    harmo.addPendulumFin(100.0,0.08,0.05,1.0,'y');
+    
+    harmo.addPendulumFin(200.0,0.04,0.01,0.0,'x');
+    harmo.addPendulumFin(200.0,0.08,0.01,1.0,'y');
+    harmo.addPendulumFin(30.0,3.0,0.01,0.5,'x');
+    harmo.addPendulumFin(60.0,3.1,0.01,1.0,'y');
     
     harmo.addPendulumFin(200.0,0.04,0.01,0.3,'x');
     harmo.addPendulumFin(200.0,0.08,0.01,1.0,'y');
@@ -43,6 +55,8 @@ style="border:1px solid #000000;">
     
     harmo.drawChange();
 </script>
-    
-</body>
-</html>
+
+Header photo &copy; i.ytimg.com/<br>
+Icon photo &copy; Sam Woolf 2018<br>
+Influences by: http://www.lukewallin.co.uk/ and 
+{: style="color:gray; font-size: 80%; text-align: left;"}
